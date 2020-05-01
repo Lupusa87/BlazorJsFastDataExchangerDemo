147f240c5fcaf1631fe40f9d3cb52a8831b40c01
Lupusa87 <VakhtangiAbashidze@gmail.com>
2020-05-01 17:49:09 -0400

    5/1/2020

---
+++ BlazorJsFastDataExchangerDemo/Shared/MainLayout.razor.cs
@@ -0,0 +1,51 @@
+﻿using BlazorCounterHelper;
+using BlazorWindowHelper;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorJsFastDataExchangerDemo.Shared
+{
+    public partial class MainLayout
+    {
+        //[Inject]
+        //HttpClient httpClient { get; set; }
+
+        [Inject]
+        NavigationManager navigationManager { get; set; }
+
+        [Inject]
+        IJSRuntime jsRuntime { get; set; }
+
+
+        protected override async Task OnInitializedAsync()
+        {
+
+
+            BWHJsInterop.jsRuntime = jsRuntime;
+
+            CounterHelper.Initialize();
+            await CounterHelper.CmdAddCounter(new TSCounter() { Source = navigationManager.Uri, Action = "visit" });
+
+            await base.OnInitializedAsync();
+
+            return;
+        }
+
+        protected override void OnAfterRender(bool firstRender)
+        {
+
+            if (firstRender)
+            {
+                BWHelperFunctions.CheckIfMobile();
+            }
+
+            base.OnAfterRender(firstRender);
+        }
+
+
+    }
+}
